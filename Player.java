d625d6b3cd76eb3a95478ee57c13e9052e424864
Anne Chepkeitany <rerimoianne.4@gmail.com>
2020-04-30 17:53:25 +0300

    Add Player class

---
+++ Player.java
@@ -0,0 +1,14 @@
+public class Player {
+
+    int id;
+    String name;
+    String team;
+
+
+    public Player(int id, String name, String team) {
+        this.id = id;
+        this.name = name;
+        this.team = team;
+    }
+
+}
